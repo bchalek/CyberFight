2456afa2659c6018d7379fa02bd4cc19a6bad3f3
root <root@debian12.linuxvmimages.local>
2025-06-15 05:25:52 -0500

    splunk

--- splunk.sh
+++ splunk.sh
@@ -1,12 +1,18 @@
 #!/bin/bash
-#dpkg -i splunkforwarder-9.4.3-237ebbd22314-linux-amd64.deb
+echo "##############Instalacja#################"
+dpkg -i splunkforwarder-9.4.3-237ebbd22314-linux-amd64.deb
+echo "######Utworzenie katalogu /opt/logs######"
 mkdir /opt/logs
+echo "#####Nadanie uprawnien###################"
 chmod 777 /opt/logs
+echo "############Przekopiowanie spl do tmp####"
 cp ./splunkclouduf.spl /tmp/
-
-/opt/splunkforwarder/bin/splunkd
+echo "#######Start splunk######################"
+#/opt/splunkforwarder/bin/splunkd
+echo "###########Instalacja spki###############"
 /opt/splunkforwarder/bin/splunk install app /tmp/splunkclouduf.spl
 
+echo "#######Utworzenie local.conf#############"
 cat << EOF >/opt/splunkforwarder/etc/system/local/input.conf
 [monitor:/opt/logs/*]
 disabled=false
@@ -15,4 +21,6 @@ initCrcLenght=65535
 index=mods
 source=mods
 EOF
+
+echo " ######Restart splunk ###################"
 /opt/splunkforwarder/bin/splunk restart
