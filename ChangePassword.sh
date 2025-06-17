76dd226d7d715cc69d54ed25ff97d5ae9d1f5781
Bartosz Chałek <bchalek@windowslive.com>
2025-06-17 08:32:52 +0200

    uspójnienie hasełek

--- ChangePassword.sh
+++ ChangePassword.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Nowe hasło
-NEW_PASSWORD="Skleroza123!"
+NEW_PASSWORD="Skleroza1234!@"
 
 # Wyszukaj użytkowników z aktywną powłoką (czyli nie /bin/false i nie /usr/sbin/nologin)
 awk -F: '$7 != "/bin/false" && $7 != "/usr/sbin/nologin"' /etc/passwd | cut -d: -f1 | while read USER; do
